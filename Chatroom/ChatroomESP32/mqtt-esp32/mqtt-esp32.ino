9200b372dcc4a02429d918698cdee660b9a985a2
giuliocorradini <39673120+giuliocorradini@users.noreply.github.com>
2019-11-22 14:56:50 +0100

    Esp32 Client MQTT
    Code for MQTT communication on ESP32 Client

---
+++ Chatroom/ChatroomESP32/mqtt-esp32/mqtt-esp32.ino
@@ -0,0 +1,28 @@
+#include <WiFi.h>
+#include <PubSubClient.h>
+
+WiFiClient wifiSTA;
+PubSubClient client;
+
+void setup() {
+  //initialize WiFi client --> wifiSTA
+  client.setClient(wifiSTA);
+  client.setServer("test.mosquitto.org", 1883);
+  while(!client.connect("esp-di-mecat")) {
+    Serial.println("Non riesco a connettermi al broker, riprovo in 5 secondi");
+    delay(5000);
+  }
+}
+
+void loop() {
+  char msg[1025];
+  //ask for message
+  publishMessage(msg);
+}
+
+int publishMessage(char* payload) {
+  char message[1080] = "";
+  sprintf(message, "{\"username\":\"mecatesp\",\"contents\":\"%1024s\",\"timestamp\":\"-1\"}", payload);
+  return client.publish("chatroom", payload);
+}
+
