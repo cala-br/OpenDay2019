9a84aef468bd4c3c7097950f2520286a6226c853
Giulio <giuliocorra@icloud.com>
2019-11-22 22:30:27 +0100

    Implemented registering

--- Chatroom/ChatroomBash/chat.sh
+++ Chatroom/ChatroomBash/chat.sh
@@ -5,22 +5,65 @@
 
 mqttHost="test.mosquitto.org"
 mqttPort=1883
-globalChat="fermi/od19/global"
+globalChat="chatroom"
 
-username=$USER
-#TODO ask for name on launch
+registerServer="register-server.fermi.mo.it"#"localhost:40000"
 
-#TODO reset from remote location
+function registerUsername {
+	uname=$1
+	response=$(curl -X POST -d "$uname" "$registerServer/register" -w %{http_code} 2> /dev/null)
+	if [ "$response" == 200 ]; then
+		echo $uname
+	else
+		uname="$uname$(date +%s)"
+		curl -X POST -d "$uname" "$registerServer/register" &> /dev/null
+		echo $uname
+	fi
+}
 
-while true; do
+clear
+
+echo -n "Come ti chiami? "
+tput smul
+read username
+username=$(registerUsername $username)
+clear
+
+function printUsername {
+	tput cup 1 1
+	tput rmul
+	tput bold
+	echo -n "Nickname: $username"
+	tput sgr0
+}
+
+function newMessage {
+	tput cup $(($(tput lines) - 1)) 0
 	echo -n '>> '
 	read text
 	if [[ "$text" == '' ]]; then
+		tput setaf 1
 		echo "Non puoi inviare un messaggio vuoto..."		
+		tput sgr0
 	else
 		message="{\"username\":\"$username\",\"contents\":\"$text\",\"timestamp\":$(date +%s)}"
 		mosquitto_pub -h "$mqttHost" -p "$mqttPort" -t "$globalChat" -m "$message"
 		echo "Messaggio inviato"
 	fi
+	echo -en "\n\n"
+}
 
+function unregisterUsername {
+	curl -X POST -d "$uname" "$registerServer/deregister"
+	tput sgr0
+	exit
+}
+
+trap unregisterUsername SIGINT
+
+#TODO reset from chatroom controller
+
+while true; do
+	printUsername
+	newMessage
 done
